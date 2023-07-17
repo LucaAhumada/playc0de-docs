99befe74925c1d6a83d1dea2b620ee30bbd3892b
Gonzalo Martinez <gonmartnez@gmail.com>
2023-07-17 17:34:48 -0300

    Fixed HomepageClients import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import HomepageClients from '@site/src/components/HomePageClients';
+import HomepageClients from '@site/src/components/HomepageClients';
 import {useColorMode} from '@docusaurus/theme-common';
 
 import styles from './index.module.css';
