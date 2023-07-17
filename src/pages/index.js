d8ef6fe11cbfadd4343a8f6ac1e84cc382f66306
Luca <lahumada@pm.me>
2023-07-17 17:56:19 -0300

    fix image height and change color mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,17 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageClients from '@site/src/components/HomepageClients';
-import {useColorMode} from '@docusaurus/theme-common';
+import { useColorMode } from '@docusaurus/theme-common';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  const {colorMode, setColorMode} = useColorMode();
+  const { siteConfig } = useDocusaurusContext();
+  const { colorMode, setColorMode } = useColorMode();
   return (
     <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <img src={colorMode === 'dark' ? "img/logo_dark.png" : "img/logo_light.png"} width={500}></img>
+        <img src={colorMode !== 'dark' ? "img/logo_light.png" : "img/logo_dark.png"} width={500} height={320}></img>
         {/* <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p> */}
         <div className={styles.buttons}>
@@ -31,7 +31,7 @@ function HomepageHeader() {
 }
 
 function Platforms() {
-  return(
+  return (
     <div className="text--center">
       <img className={styles.platforms} src="https://playwright.dev/img/logos/Browsers.png" width={'40%'}></img>
     </div>
@@ -39,7 +39,7 @@ function Platforms() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Testing, made easy | ${siteConfig.title}`}
